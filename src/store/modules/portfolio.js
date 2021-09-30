a0d0b6fb0d9bc6d7dae0e57848b876efb0db8193
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-29 22:58:17 -0300

    creating actions and getters and returning total funds

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -28,5 +28,26 @@ export default {
             }
             state.funds += stockPrice * quantity
         }
+    },
+    actions:{
+        sellStock({commit},order){
+            commit('sellStock',order)
+        }
+    },
+    getters:{
+        stockPortfolio(state,getters){
+            return state.stocks.map(stock=>{
+                const record = getters.stocks.find(element.id == stock.id)
+                return{
+                    id:stock.id,
+                    quantity:stock.quantity,
+                    name:record.name,
+                    price:record.price
+                }
+            })
+        },
+        funds(state){
+            return state.funds
+        }
     }
 }
