372b646e27bcd0921801b5ba25cf57b4576e4297
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-30 18:08:03 -0300

    layout funds done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1 class="display-3">Negocie e Consulte suas Ações</h1>
-    <v-sheet color="primary">
-        <v-icon>info</v-icon>
-        <span>Você pode salvar e carregar os dados</span>
+    <h1 class="display-3 font-weight-light mb-4">Negocie e Consulte suas Ações</h1>
+    <v-sheet :elevation="6" class="pa-2 blue darken-1">
+        <v-icon class="white--text mr-3">info</v-icon>
+        <span class="headline white--text font-weight-light">Você pode salvar & carregar os dados</span>
     </v-sheet>
     <v-divider class="my-4"></v-divider>
     <p><strong>Seu Saldo:</strong>{{ funds }}</p>
