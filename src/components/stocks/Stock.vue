be7dcee0107bf723dcff6aa362eee61784407fef
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-30 18:19:17 -0300

    setting validation for insuffienct funds

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -9,7 +9,7 @@
             <v-container fill-height>
                 <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
                 <v-btn class="green darken-3 white--text" 
-                :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                :disabled="insufficientFunds ||quantity <= 0 || !Number.isInteger(quantity)"
                 @click="buyStock">Comprar</v-btn>
             </v-container>
         </v-card>
@@ -22,6 +22,14 @@ export default {
     data(){
         return{
             quantity:0
+        }
+    },
+    computed:{
+        funds(){
+            return this.$store.getters.funds
+        },
+        insufficientFunds(){
+            return this.quantity * this.stock.price > this.funds
         }
     },
     methods:{
