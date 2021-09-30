45302fabe2de00e8fe8d1e0e45c6c346f4d5ba8d
Isaac Souza <isaacsouzawrk@gmail.com>
2021-09-30 18:02:44 -0300

    building home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1 class="display-3">Negocie e Consulte suas Ações</h1>
+    <v-sheet color="primary">
+        <v-icon>info</v-icon>
+        <span>Você pode salvar e carregar os dados</span>
+    </v-sheet>
+    <v-divider class="my-4"></v-divider>
+    <p><strong>Seu Saldo:</strong>{{ funds }}</p>
+  </div>
 </template>
 
 <script>
